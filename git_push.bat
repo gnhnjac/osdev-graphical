bf1603654af3afcd6ab14f6e5409f07499fd32c9
עמי קרייזל <gnhnjac@gmail.com>
2023-03-09 21:36:04 +0200

    03_09_2023

---
+++ git_push.bat
@@ -0,0 +1,10 @@
+git add .
+for /f "tokens=1-4 delims=/ " %%i in ("%date%") do (
+     set dow=%%i
+     set month=%%j
+     set day=%%k
+     set year=%%l
+)
+set datestr=%month%_%day%_%year%
+git commit -m "%datestr%"
+git push
